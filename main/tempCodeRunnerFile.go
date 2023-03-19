aabe2048beff150c79b911331f2d15940f526706
Eldar <eldar.sharapiev@gmail.com>
2023-03-19 07:17:07 +0600

    Added Midterm

---
+++ main/tempCodeRunnerFile.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/ToRnaDo1337/RadarClothes/login"
+	"github.com/ToRnaDo1337/RadarClothes/registration"
+	"github.com/ToRnaDo1337/RadarClothes/shop"
+)
+
+func main() {
+	http.HandleFunc("/register", registration.Register)
+	http.HandleFunc("/login", login.Login)
+	http.HandleFunc("/RadarClothes/rate", shop.RateHandler)
+	http.HandleFunc("/addingItems", shop.HandleIndex)
+	http.HandleFunc("/RadarClothes", shop.HandleItems)
+	http.ListenAndServe(":8080", nil)
+}
